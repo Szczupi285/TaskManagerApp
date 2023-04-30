ffbaf09d87e42aa2351b9b930c9a74fb561b089c
Szczupi285 <bartekbiexd@gmail.com>
2023-04-30 16:13:45 +0200

    initial frontend for TaskManager

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TaskManagerApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void RegisterSend_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void LoginSend_Click(object sender, RoutedEventArgs e)
+        {
+            // if(login = databaseLogin && password = databasePassoword)
+            // open new window
+            TaskWindow window = new TaskWindow();
+            window.Show();
+            this.Close();
+            // else wrong login or password popup
+        }
+    }
+}
---
+++ TaskWindow.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace TaskManagerApp
+{
+    /// <summary>
+    /// Logika interakcji dla klasy TaskWindow.xaml
+    /// </summary>
+    public partial class TaskWindow : Window
+    {
+        public TaskWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void OnWindowLoaded(object sender, RoutedEventArgs e)
+        {
+            for(int i = 0; i < 20; i++)
+            {
+                // Sets the rowDefinition heigth automaticly
+                RowDefinition rowDef = new RowDefinition();
+                rowDef.Height = new GridLength(1, GridUnitType.Auto);
+                CurrentNotes.RowDefinitions.Add(rowDef);
+
+                TextBlock Note = new TextBlock();
+                // read value from database
+                Note.Text = $"";
+                Note.Margin = new Thickness(10);
+                
+                Note.TextWrapping = TextWrapping.Wrap;
+                // creates the frame and assign the properties
+                Frame frame = new Frame();
+                frame.Content = Note;
+                frame.Margin = new Thickness(10);
+                frame.BorderThickness = new Thickness(2);
+                frame.BorderBrush = Brushes.Black;
+                Grid.SetRow(frame, i);
+
+                
+                CurrentNotes.Children.Add(frame);
+                
+            }
+            
+        }
+    }
+}
