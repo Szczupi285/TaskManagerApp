011cb2694011de5c799a31abf512c16e9a6eb9da
Szczupi285 <bartekbiexd@gmail.com>
2023-05-13 11:51:07 +0200

    Task history window, navigation buttons

---
+++ History.xaml.cs
@@ -0,0 +1,216 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using TaskManagerApp;
+
+namespace TaskManagerApp
+{
+    /// <summary>
+    /// Logika interakcji dla klasy History.xaml
+    /// </summary>
+    public partial class History : Window
+    {
+        public int Id { get; init; }
+        public string Login { get; init; }
+        public string Password;
+
+        // to do change value in database in setters
+        public bool ShowAllHistory { get; set; }
+        public bool ShowFinishedHistory { get; set; }
+        public bool ShowUnfinishedHistory { get; set; }
+
+        public History(int id, string login , string password)
+        {
+            InitializeComponent();
+            this.Id = id;
+            this.Login = login;
+            this.Password = password;
+            LoadHistoryFromDB();
+        }
+        public void OpenAndCloseConn(Action<SqlConnection> action)
+        {
+            string connectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=E:\repository\TaskManagerApp\Database1.mdf;Integrated Security=True";
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                action(connection);
+                connection.Close();
+            }
+        }
+
+        private void LoadHistoryFromDB()
+        {
+            string connectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=E:\repository\TaskManagerApp\Database1.mdf;Integrated Security=True";
+            SqlConnection connection = new SqlConnection(connectionString);
+            connection.Open();
+            SqlCommand ReadNotes;
+
+            if(ShowAllHistory)
+                ReadNotes = new SqlCommand($"SELECT Note, Deadline, IsFinished FROM Notes WHERE UserID = '{Id}' " +
+                    $"AND (Deadline < CONVERT(DATETIME, GETDATE(), 101) OR IsFinished = '1' OR IsFailed = '1')", connection);
+            else if(ShowFinishedHistory)
+            {
+                ReadNotes = new SqlCommand($"SELECT Note, Deadline FROM Notes WHERE UserID = '{Id}' " +
+                    $"AND IsFinished = '1'", connection);
+            }
+            else
+            {
+                ReadNotes = new SqlCommand($"SELECT Note, Deadline FROM Notes WHERE UserID = '{Id}' AND IsFinished = '0'" +
+                    $"AND (Deadline < CONVERT(DATETIME, GETDATE(), 101) OR IsFailed = '1')", connection);
+
+            }
+            
+            SqlDataReader readerNotes = ReadNotes.ExecuteReader();
+            
+
+
+
+            // for record in database that date hasn't expired yet
+            int iterator = 0;
+            while (readerNotes.Read())
+            {
+
+                RowDefinition rowDef = new RowDefinition();
+                rowDef.Height = new GridLength(1, GridUnitType.Auto);
+                CurrentNotes.RowDefinitions.Add(rowDef);
+
+                TextBlock Note = new TextBlock();
+
+                // read value from database
+                Note.Text = $"Task: {readerNotes["Note"]} \nDeadline: {((DateTime)readerNotes["Deadline"]).ToString("yyyy/MM/dd")}";
+                // assing the id of note to FinBtn name so we get the id of the exact note next to button
+                // we need to append "A" because Name of the button can't be only the number
+                
+
+                Note.Margin = new Thickness(10);
+                Note.TextWrapping = TextWrapping.Wrap;
+
+                // creates the frame and assign the properties
+                Frame frame = new Frame();
+                frame.Content = Note;
+                frame.Margin = new Thickness(10);
+                frame.BorderThickness = new Thickness(2);
+                frame.BorderBrush = Brushes.Black;
+
+                // display note with green backgroud if the task was finished and red if it wasn't
+                
+                if (ShowAllHistory)
+                {
+                    int isFinished = Convert.ToInt32(readerNotes["IsFinished"]);
+                    if (isFinished == 1)
+                        frame.Background = new SolidColorBrush(Color.FromArgb(128, 0, 255, 0));
+                    else
+                    {
+                        frame.Background = new SolidColorBrush(Color.FromArgb(128, 255, 0, 0));
+                    }
+                }
+                else if(ShowFinishedHistory)
+                {
+                    frame.Background = new SolidColorBrush(Color.FromArgb(128, 0, 255, 0));
+                }
+                else
+                {
+                    frame.Background = new SolidColorBrush(Color.FromArgb(128, 255, 0, 0));
+                }
+
+
+                Grid.SetRow(frame, iterator);
+                
+
+                CurrentNotes.Children.Add(frame);
+                
+
+                iterator++;
+            }
+
+
+            connection.Close();
+        }
+
+        #region settings
+        private void HistorySettings_Click(object sender, RoutedEventArgs e)
+        {
+            if (Checkboxes.Visibility == Visibility.Hidden)
+                Checkboxes.Visibility = Visibility.Visible;
+            else
+                Checkboxes.Visibility = Visibility.Hidden;
+        }
+
+        private void TaskWindow_Click(object sender, RoutedEventArgs e)
+        {
+            
+            TaskWindow taskwindow = new TaskWindow(Id, Login, Password);
+            this.Close();
+            taskwindow.Show();
+
+        }
+
+
+
+        private void RadioButton_Checked_1(object sender, RoutedEventArgs e)
+        {
+            OpenAndCloseConn((connection) => {
+                SqlCommand HighlightChecked = new SqlCommand($"Update Settings Set ShowAllHis = '1', ShowFinHis = '0'," +
+                    $" ShowUnfinHis = '0' Where UserId = '{Id}'", connection);
+
+                ShowAllHistory = true;
+                ShowFinishedHistory = false;
+                ShowUnfinishedHistory = false;
+                HighlightChecked.ExecuteNonQuery();
+            });
+            Refresh_Click(sender, e);
+
+        }
+
+        private void RadioButton_Checked_2(object sender, RoutedEventArgs e)
+        {
+            OpenAndCloseConn((connection) => {
+                SqlCommand HighlightChecked = new SqlCommand($"Update Settings Set ShowAllHis = '0', ShowFinHis = '1'," +
+                    $" ShowUnfinHis = '0' Where UserId = '{Id}'", connection);
+
+                ShowAllHistory = false;
+                ShowFinishedHistory = true;
+                ShowUnfinishedHistory = false;
+
+                HighlightChecked.ExecuteNonQuery();
+            });
+            Refresh_Click(sender, e);
+
+        }
+
+        private void RadioButton_Checked_3(object sender, RoutedEventArgs e)
+        {
+            OpenAndCloseConn((connection) => {
+                SqlCommand HighlightChecked = new SqlCommand($"Update Settings Set ShowAllHis = '0', ShowFinHis = '0', " +
+                    $"ShowUnfinHis = '1' Where UserId = '{Id}'", connection);
+
+                ShowAllHistory = false;
+                ShowFinishedHistory = false;
+                ShowUnfinishedHistory = true;
+
+                HighlightChecked.ExecuteNonQuery();
+            });
+            Refresh_Click(sender, e);   
+        }
+
+        #endregion
+
+
+        private void Refresh_Click(object sender, RoutedEventArgs e)
+        {
+            CurrentNotes.Children.Clear();
+            LoadHistoryFromDB();
+        }
+    }
+}
--- TaskWindow.xaml.cs
+++ TaskWindow.xaml.cs
@@ -17,6 +17,8 @@ using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 using System.Windows.Shell;
 using System.Net.Mail;
+using System.Reflection;
+using System.Security.Principal;
 
 namespace TaskManagerApp
 {
@@ -26,9 +28,13 @@ namespace TaskManagerApp
     public partial class TaskWindow : Window
     {
         
-        public int Id { get; set; }
-        public string Login { get; set; }
-        public string Password;
+        public int Id { get; init; }
+        public string Login { get; init; }
+        public string Password { get; init; }
+        // to do make settings properties and change value in database in setters
+
+
+
         public TaskWindow(int id, string login, string password)
         {
             this.Id = id;
@@ -85,7 +91,7 @@ namespace TaskManagerApp
             
         }
 
-        private void OpenAndCloseConn(Action<SqlConnection> action)
+        public void OpenAndCloseConn(Action<SqlConnection> action)
         {
             string connectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=E:\repository\TaskManagerApp\Database1.mdf;Integrated Security=True";
             using (SqlConnection connection = new SqlConnection(connectionString))
@@ -105,11 +111,11 @@ namespace TaskManagerApp
 
             if(Sort.IsChecked == true)
             {
-                ReadNotes = new SqlCommand($"SELECT Note, Deadline FROM Notes WHERE UserID = '{Id}' AND Deadline > CONVERT(DATETIME, GETDATE(), 101) Order BY Deadline ASC", connection);
+                ReadNotes = new SqlCommand($"SELECT Note, Deadline , Id FROM Notes WHERE UserID = '{Id}' AND Deadline > CONVERT(DATETIME, GETDATE(), 101) AND IsFinished = '0' AND IsFailed = '0' Order BY Deadline ASC", connection);
             }
             else
             {
-                ReadNotes = new SqlCommand($"SELECT Note, Deadline FROM Notes WHERE UserID = '{Id}' AND Deadline > CONVERT(DATETIME, GETDATE(), 101)", connection);
+                ReadNotes = new SqlCommand($"SELECT Note, Deadline, Id FROM Notes WHERE UserID = '{Id}' AND Deadline > CONVERT(DATETIME, GETDATE(), 101) AND IsFinished = '0' AND IsFailed = '0'", connection);
                 
             }
             SqlDataReader readerNotes = ReadNotes.ExecuteReader();
@@ -126,17 +132,44 @@ namespace TaskManagerApp
                 CurrentNotes.RowDefinitions.Add(rowDef);
 
                 TextBlock Note = new TextBlock();
+                
+                // creates the finish button and assign the properties
+                Button FinBtn = new Button();
+                FinBtn.Content = "\u2713";
+                FinBtn.Width = 20;
+                FinBtn.Height = 20;
+                FinBtn.VerticalAlignment = VerticalAlignment.Top;
+                FinBtn.HorizontalAlignment = HorizontalAlignment.Left;
+                FinBtn.Click += new RoutedEventHandler(FinBtn_Click);
+
+                // create the Fail button
+                Button FailBtn = new Button();
+                FailBtn.Content = "\u2717";
+                FailBtn.Width = 20;
+                FailBtn.Height = 20;
+                FailBtn.VerticalAlignment = VerticalAlignment.Bottom;
+                FailBtn.HorizontalAlignment = HorizontalAlignment.Left;
+                FailBtn.Click += new RoutedEventHandler(FailBtn_Click);
+
+
                 // read value from database
                 Note.Text = $"Task: {readerNotes["Note"]} \nDeadline: {((DateTime)readerNotes["Deadline"]).ToString("yyyy/MM/dd")}";
-                Note.Margin = new Thickness(10);
+                // assing the id of note to FinBtn name so we get the id of the exact note next to button
+                // we need to append "A" because Name of the button can't be only the number
+                var x = "A" + readerNotes["Id"].ToString();
+                FinBtn.Name = x;
+                FailBtn.Name = x;
 
+                Note.Margin = new Thickness(10);
                 Note.TextWrapping = TextWrapping.Wrap;
+
                 // creates the frame and assign the properties
                 Frame frame = new Frame();
                 frame.Content = Note;
                 frame.Margin = new Thickness(10);
                 frame.BorderThickness = new Thickness(2);
                 frame.BorderBrush = Brushes.Black;
+                
                 // gets the difference of deadline and current time
                 TimeSpan difference = (DateTime)readerNotes["Deadline"] - DateTime.Now;
                 // checks if deadline is less than in 3 days and if highlight setting is turned on
@@ -144,14 +177,17 @@ namespace TaskManagerApp
                     frame.Background = new SolidColorBrush(Color.FromArgb(128, 255, 0, 0));
 
                 Grid.SetRow(frame, iterator);
-                iterator++;
-
+                Grid.SetRow(FinBtn, iterator);
+                Grid.SetRow(FailBtn, iterator);
 
                 CurrentNotes.Children.Add(frame);
+                CurrentNotes.Children.Add(FinBtn);
+                CurrentNotes.Children.Add(FailBtn);
 
+                iterator++;
             }
 
-           
+
             connection.Close();
         }
 
@@ -223,6 +259,7 @@ namespace TaskManagerApp
                 SqlCommand HighlightChecked = new SqlCommand($"Update Settings Set DeadlineSetting = '1' Where UserId = '{Id}'", connection);
                 HighlightChecked.ExecuteNonQuery();
             });
+            Refresh_Click(sender, e);
         }
 
         private void Highlight_Unchecked(object sender, RoutedEventArgs e)
@@ -231,6 +268,7 @@ namespace TaskManagerApp
                 SqlCommand HighlightChecked = new SqlCommand($"Update Settings Set DeadlineSetting = '0' Where UserId = '{Id}'", connection);
                 HighlightChecked.ExecuteNonQuery();
             });
+            Refresh_Click(sender, e);
 
         }
 
@@ -240,6 +278,8 @@ namespace TaskManagerApp
                 SqlCommand HighlightChecked = new SqlCommand($"Update Settings Set SendRemainder = '1' Where UserId = '{Id}'", connection);
                 HighlightChecked.ExecuteNonQuery();
             });
+            Refresh_Click(sender, e);
+
         }
         private void Remainder_Unchecked(object sender, RoutedEventArgs e)
         {
@@ -247,6 +287,8 @@ namespace TaskManagerApp
                 SqlCommand HighlightChecked = new SqlCommand($"Update Settings Set SendRemainder = '0' Where UserId = '{Id}'", connection);
                 HighlightChecked.ExecuteNonQuery();
             });
+            Refresh_Click(sender, e);
+
         }
 
         private void Sort_Checked(object sender, RoutedEventArgs e)
@@ -255,6 +297,8 @@ namespace TaskManagerApp
                 SqlCommand HighlightChecked = new SqlCommand($"Update Settings Set SortByDeadline = '1' Where UserId = '{Id}'", connection);
                 HighlightChecked.ExecuteNonQuery();
             });
+            Refresh_Click(sender, e);
+
 
         }
 
@@ -264,6 +308,7 @@ namespace TaskManagerApp
                 SqlCommand HighlightChecked = new SqlCommand($"Update Settings Set SortByDeadline = '0' Where UserId = '{Id}'", connection);
                 HighlightChecked.ExecuteNonQuery();
             });
+            Refresh_Click(sender, e);
 
         }
 
@@ -274,5 +319,55 @@ namespace TaskManagerApp
             CurrentNotes.Children.Clear();
             LoadNotesFromDB();
         }
+
+        private void FinBtn_Click(object sender, RoutedEventArgs e)
+        {
+            // cast the sender parameter to a Button object
+            Button? clickedButton = sender as Button;
+
+            // gets the name of specified button so we can get to the exact one 
+            // removes the unrequired number
+            string NoteID = clickedButton!.Name.Replace('A',' ').Trim();
+            OpenAndCloseConn((connection) => { 
+                SqlCommand IsFinished = new SqlCommand($"Update Notes Set IsFinished = '1' Where UserId = '{Id}' AND Id = '{NoteID}'",connection);
+                IsFinished.ExecuteNonQuery();
+            });
+
+            Refresh_Click(sender, e);
+
+
+        }
+
+        private void FailBtn_Click(object sender, RoutedEventArgs e)
+        {
+            // cast the sender parameter to a Button object
+            Button? clickedButton = sender as Button;
+
+            // gets the name of specified button so we can get to the exact one 
+            // removes the unrequired number
+            string NoteID = clickedButton!.Name.Replace('A', ' ').Trim();
+            OpenAndCloseConn((connection) => {
+                SqlCommand IsFailed = new SqlCommand($"Update Notes Set IsFailed = '1' Where UserId = '{Id}' AND Id = '{NoteID}'", connection);
+                IsFailed.ExecuteNonQuery();
+            });
+
+            Refresh_Click(sender, e);
+
+        }
+
+        private void LoginPage_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow mainWindow = new MainWindow();
+            this.Close();
+            mainWindow.Show();
+            
+        }
+
+        private void History_Click(object sender, RoutedEventArgs e)
+        {
+            History history = new History(Id, Login, Password);
+            this.Close();
+            history.Show();
+        }
     }
-}
+    }
